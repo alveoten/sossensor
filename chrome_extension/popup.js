59498dd93c0acedd2db57cd5f5ed26d0c3a3863c
Luca Agnello <luca@gnello.com>
2017-09-20 09:24:37 +0200

    added styles

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -4,6 +4,19 @@ chrome.runtime.sendMessage({data:"Handshake"},function(response){
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        document.getElementById("status").innerHTML = request.msg;
+        var status = $("#status");
+        status.innerHTML = request.msg;
+        status.removeClass(status.attr('class')).addClass("label " + request.className);
+
+        $("#refresh").find("i").removeClass("faa-spin animated");
     }
 );
+
+var background = chrome.extension.getBackgroundPage();
+
+$(document).ready(function() {
+    $("#refresh").click(function() {
+        background.doRequest();
+        $(this).find("i").addClass("faa-spin animated");
+    });
+});
